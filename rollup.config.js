0e3552410fcbf039a07c6dba8b9e733fdc93bf16
jagnani73 <yashjagnani73@gmail.com>
2021-06-08 11:34:02 +0530

    ✨ rollup

---
+++ rollup.config.js
@@ -0,0 +1,24 @@
+import typescript from "rollup-plugin-typescript2";
+import postcss from "rollup-plugin-postcss";
+
+import pkg from "./package.json";
+
+export default {
+  input: "src/index.tsx",
+  output: [
+    {
+      file: pkg.main,
+      format: "cjs",
+      exports: "named",
+      sourcemap: true,
+      strict: false,
+    },
+  ],
+  plugins: [
+    typescript({ objectHashIgnoreUnknownHack: true }),
+    postcss({
+      plugins: [],
+    }),
+  ],
+  external: ["react", "react-dom"],
+};
